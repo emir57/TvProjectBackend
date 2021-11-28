b7595c539df1b0f9e504006c92845be8ac5384c3
emir57 <emir.gurbuz06@hotmail.com>
2021-11-28 17:03:04 +0300

    Configuring DataAccess Layer

---
+++ DataAccess/Concrete/EntityFramework/TvProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class TvProjectContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TvProjectDb;integrated security=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tv> Tvs { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<TvBrand> TvBrands { get; set; }
+        public DbSet<TvPhoto> TvPhotos { get; set; }
+    }
+}
