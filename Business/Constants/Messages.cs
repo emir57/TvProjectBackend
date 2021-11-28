e211002ed2e03731b72e25da4955461bc2fa59c0
emir57 <emir.gurbuz06@hotmail.com>
2021-11-28 17:58:18 +0300

    Add Constants

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string SuccessAdd = "Ekleme Başarılı";
+        public static string SuccessDelete = "Silme Başarılı";
+        public static string SuccessUpdate = "Güncelleme Başarılı";
+        public static string SuccessGet = "Listeleme Başarılı";
+    }
+}
