38dee9a39cf3a56a9bee78099c57049be1d66cc1
emir57 <emir.gurbuz06@hotmail.com>
2021-11-28 17:29:02 +0300

    Add Tables And DataAccess

---
+++ Entities/Concrete/UserCreditCard.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class UserCreditCard:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Date { get; set; }
+    }
+}
