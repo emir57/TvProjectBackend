03d9f0e806346153b489be9af5ca32b746198bbe
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 16:09:41 +0300

    Add Aspects

--- Business/Concrete/TvManager.cs
+++ Business/Concrete/TvManager.cs
@@ -1,5 +1,9 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.Validators.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,25 +23,30 @@ namespace Business.Concrete
         {
             _tvDal = tvDal;
         }
-
+        [ValidationAspect(typeof(TvValidator))]
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Add(Tv entity)
         {
             await _tvDal.Add(entity);
             return new SuccessResult(Messages.SuccessAdd);
         }
-
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Delete(Tv entity)
         {
             await _tvDal.Delete(entity);
             return new SuccessResult(Messages.SuccessDelete);
         }
-
+        [CacheAspect]
+        [PerformanceAspect(5)]
         public async Task<IDataResult<Tv>> Get(Expression<Func<Tv, bool>> filter)
         {
             var result = await _tvDal.Get(filter);
             return new SuccessDataResult<Tv>(result, Messages.SuccessGet);
         }
-
+        [CacheAspect]
+        [PerformanceAspect(5)]
         public async Task<IDataResult<List<Tv>>> GetAll(Expression<Func<Tv, bool>> filter = null)
         {
             var result = filter == null ?
@@ -52,7 +61,9 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Tv>>(result, Messages.SuccessGet);
             
         }
-
+        [ValidationAspect(typeof(TvValidator))]
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Update(Tv entity)
         {
             await _tvDal.Update(entity);
