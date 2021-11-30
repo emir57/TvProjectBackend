bc47dc8e974e087def3175b2cf04aa8d2aaa962e
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:46:23 +0300

    CityValidator

--- Business/Validators/FluentValidation/CityValidator.cs
+++ Business/Validators/FluentValidation/CityValidator.cs
@@ -8,5 +8,11 @@ namespace Business.Validators.FluentValidation
 {
     public class CityValidator:AbstractValidator<City>
     {
+        public CityValidator()
+        {
+            RuleFor(c => c.CityName).NotEmpty();
+            RuleFor(c => c.CityName).MinimumLength(1);
+            RuleFor(c => c.CityName).MaximumLength(70);
+        }
     }
 }
