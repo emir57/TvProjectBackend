7401cc55974c4ecb00dd3ae7380ce5d053186903
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:51:33 +0300

    UserAddressValidator

--- Business/Validators/FluentValidation/UserAddressValidator.cs
+++ Business/Validators/FluentValidation/UserAddressValidator.cs
@@ -8,5 +8,12 @@ namespace Business.Validators.FluentValidation
 {
     public class UserAddressValidator:AbstractValidator<UserAddress>
     {
+        public UserAddressValidator()
+        {
+            RuleFor(u => u.CityId).NotEmpty();
+            RuleFor(u => u.AddressText).NotEmpty();
+            RuleFor(u => u.AddressText).MaximumLength(250);
+            RuleFor(u => u.UserId).NotEmpty();
+        }
     }
 }
