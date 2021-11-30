f04d74ace6bee58450af12bf598fab64a494a58a
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:53:39 +0300

    UserValidator

--- Business/Validators/FluentValidation/UserValidator.cs
+++ Business/Validators/FluentValidation/UserValidator.cs
@@ -10,7 +10,16 @@ namespace Business.Validators.FluentValidation
     {
         public UserValidator()
         {
-            //RuleFor()
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MinimumLength(1);
+            RuleFor(u => u.FirstName).MaximumLength(50);
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.LastName).MinimumLength(1);
+            RuleFor(u => u.LastName).MaximumLength(50);
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).MinimumLength(5);
+            RuleFor(u => u.Email).MaximumLength(50);
+            RuleFor(u => u.Email).EmailAddress();
         }
     }
 }
