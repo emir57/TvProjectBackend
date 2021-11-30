f0dfc5c945936f2b96344a7ee001b307fed4caa2
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:56:04 +0300

    UserCreditCardValidator

---
+++ Business/Validators/FluentValidation/UserCreditCardValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Validators.FluentValidation
+{
+    public class UserCreditCardValidator:AbstractValidator<UserCreditCard>
+    {
+        public UserCreditCardValidator()
+        {
+            RuleFor(u => u.UserId).NotEmpty();
+            RuleFor(u => u.CreditCardNumber).NotEmpty();
+            RuleFor(u => u.CreditCardNumber).CreditCard();
+            RuleFor(u => u.CVV).NotEmpty();
+            RuleFor(u => u.CVV).MaximumLength(4);
+            RuleFor(u => u.Date).MaximumLength(5);
+        }
+    }
+}
