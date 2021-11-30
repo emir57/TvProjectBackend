da05a49a5b91f48843ec9965b9c721718c28de3f
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:27:09 +0300

    add tvphotomanager operations

--- Business/Concrete/TvPhotoManager.cs
+++ Business/Concrete/TvPhotoManager.cs
@@ -1,11 +1,55 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
     public class TvPhotoManager:ITvPhotoService
     {
+        private readonly ITvPhotoDal _tvPhotoDal;
+
+        public TvPhotoManager(ITvPhotoDal tvPhotoDal)
+        {
+            _tvPhotoDal = tvPhotoDal;
+        }
+
+        public async Task<IResult> Add(TvPhoto entity)
+        {
+            await _tvPhotoDal.Add(entity);
+            return new SuccessResult(Messages.SuccessAdd);
+        }
+
+        public async Task<IResult> Delete(TvPhoto entity)
+        {
+            await _tvPhotoDal.Delete(entity);
+            return new SuccessResult(Messages.SuccessDelete);
+        }
+
+        public async Task<IDataResult<TvPhoto>> Get(Expression<Func<TvPhoto, bool>> filter)
+        {
+            var result = await _tvPhotoDal.Get(filter);
+            return new SuccessDataResult<TvPhoto>(result, Messages.SuccessGet);
+        }
+
+        public async Task<IDataResult<List<TvPhoto>>> GetAll(Expression<Func<TvPhoto, bool>> filter = null)
+        {
+            return filter == null ?
+                new SuccessDataResult<List<TvPhoto>>(await _tvPhotoDal.GetAll(), Messages.SuccessGet) :
+                new SuccessDataResult<List<TvPhoto>>(await _tvPhotoDal.GetAll(filter), Messages.SuccessGet);
+
+        }
+
+        public async Task<IResult> Update(TvPhoto entity)
+        {
+            await _tvPhotoDal.Update(entity);
+            return new SuccessResult(Messages.SuccessUpdate);
+        }
     }
 }
