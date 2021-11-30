1fb1503e1aad2caf3b855ae7ef19738593a93515
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 16:10:35 +0300

    Add Aspects

--- Business/Concrete/TvPhotoManager.cs
+++ Business/Concrete/TvPhotoManager.cs
@@ -1,5 +1,9 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.Validators.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,25 +23,29 @@ namespace Business.Concrete
         {
             _tvPhotoDal = tvPhotoDal;
         }
-
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Add(TvPhoto entity)
         {
             await _tvPhotoDal.Add(entity);
             return new SuccessResult(Messages.SuccessAdd);
         }
-
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Delete(TvPhoto entity)
         {
             await _tvPhotoDal.Delete(entity);
             return new SuccessResult(Messages.SuccessDelete);
         }
-
+        [CacheAspect]
+        [PerformanceAspect(3)]
         public async Task<IDataResult<TvPhoto>> Get(Expression<Func<TvPhoto, bool>> filter)
         {
             var result = await _tvPhotoDal.Get(filter);
             return new SuccessDataResult<TvPhoto>(result, Messages.SuccessGet);
         }
-
+        [CacheAspect]
+        [PerformanceAspect(3)]
         public async Task<IDataResult<List<TvPhoto>>> GetAll(Expression<Func<TvPhoto, bool>> filter = null)
         {
             var result = filter == null ?
@@ -45,7 +53,8 @@ namespace Business.Concrete
                 await _tvPhotoDal.GetAll(filter);
             return new SuccessDataResult<List<TvPhoto>>(result, Messages.SuccessGet);
         }
-
+        [CacheRemoveAspect("Get")]
+        [PerformanceAspect(3)]
         public async Task<IResult> Update(TvPhoto entity)
         {
             await _tvPhotoDal.Update(entity);
