3f806861ca4ce2609da1ce51cab89453f8d06d8b
emir57 <emir.gurbuz06@hotmail.com>
2021-11-30 15:50:24 +0300

    TvDalidator

--- Business/Validators/FluentValidation/TvValidator.cs
+++ Business/Validators/FluentValidation/TvValidator.cs
@@ -10,7 +10,21 @@ namespace Business.Validators.FluentValidation
     {
         public TvValidator()
         {
-
+            RuleFor(t => t.ProductName).NotEmpty();
+            RuleFor(t => t.ProductName).MinimumLength(1);
+            RuleFor(t => t.ProductName).MaximumLength(50);
+            RuleFor(t => t.ProductCode).NotEmpty();
+            RuleFor(t => t.ProductCode).MinimumLength(1);
+            RuleFor(t => t.ProductCode).MaximumLength(50);
+            RuleFor(t => t.ScreenType).NotEmpty();
+            RuleFor(t => t.ScreenType).MinimumLength(1);
+            RuleFor(t => t.ScreenType).MaximumLength(50);
+            RuleFor(t => t.ScreenInch).NotEmpty();
+            RuleFor(t => t.ScreenInch).MaximumLength(10);
+            RuleFor(t => t.Extras).MaximumLength(50);
+            RuleFor(t => t.BrandId).NotEmpty();
+            RuleFor(t => t.UnitPrice).NotEmpty();
+            RuleFor(t => t.UnitPrice).GreaterThan(0);
         }
     }
 }
