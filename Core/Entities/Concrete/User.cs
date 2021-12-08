1a5036b29acf20ed318e519db85205518adc658a
emir57 <emir.gurbuz06@hotmail.com>
2021-12-08 14:09:50 +0300

    User Table add Column 'Key'

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -13,6 +13,7 @@ namespace Core.Entities.Concrete
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public bool Status { get; set; }
+        public string Key { get; set; }
 
     }
 }
