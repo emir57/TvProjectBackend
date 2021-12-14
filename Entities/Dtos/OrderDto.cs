5005ba33d72b8620d5c4dcd5f0940841359d13dd
emir57 <emir.gurbuz06@hotmail.com>
2021-12-14 16:28:27 +0300

    Order Dto

--- Entities/Dtos/OrderDto.cs
+++ Entities/Dtos/OrderDto.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Entities;
+using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +9,10 @@ namespace Entities.Dtos
 {
     public class OrderDto:IDto
     {
+        public User User { get; set; }
+        public Tv Tv { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public UserAddress Address { get; set; }
     }
 }
