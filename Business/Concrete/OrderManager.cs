30a1880e9d60c5aed16a07d558a6322ae4ce136c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-14 23:11:59 +0300

    Add Aspect for OrderManager

--- Business/Concrete/OrderManager.cs
+++ Business/Concrete/OrderManager.cs
@@ -1,5 +1,9 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -20,25 +24,34 @@ namespace Business.Concrete
         {
             _orderDal = orderDal;
         }
-
+        [SecuredOperation("User,Admin")]
+        //[ValidationAspect(typeof())]
+        [CacheRemoveAspect("IOrderService.Get")]
+        [PerformanceAspect(5)]
         public async Task<IResult> Add(Order entity)
         {
             await _orderDal.Add(entity);
             return new SuccessResult(Messages.SuccessAdd);
         }
-
+        [SecuredOperation("User,Admin")]
+        [CacheRemoveAspect("IOrderService.Get")]
+        [PerformanceAspect(5)]
         public async Task<IResult> Delete(Order entity)
         {
             await _orderDal.Delete(entity);
             return new SuccessResult(Messages.SuccessDelete);
         }
-
+        [SecuredOperation("User,Admin")]
+        [CacheAspect]
+        [PerformanceAspect(8)]
         public async Task<IDataResult<Order>> Get(Expression<Func<Order, bool>> filter)
         {
             var result = await _orderDal.Get(filter);
             return new SuccessDataResult<Order>(result, Messages.SuccessGet);
         }
-
+        [SecuredOperation("Admin")]
+        [CacheAspect]
+        [PerformanceAspect(8)]
         public async Task<IDataResult<List<Order>>> GetAll(Expression<Func<Order, bool>> filter = null)
         {
             var result = filter == null ?
@@ -46,13 +59,18 @@ namespace Business.Concrete
                 await _orderDal.GetAll(filter);
             return new SuccessDataResult<List<Order>>(result, Messages.SuccessGet);
         }
-
+        [SecuredOperation("User,Admin")]
+        [CacheAspect]
+        [PerformanceAspect(8)]
         public async Task<IDataResult<List<OrderDto>>> GetOrdersByUserId(int userId)
         {
             var result = await _orderDal.GetOrdersByUserId(userId);
             return new SuccessDataResult<List<OrderDto>>(result, Messages.SuccessGet);
         }
-
+        [SecuredOperation("User,Admin")]
+        //[ValidationAspect(typeof())]
+        [CacheRemoveAspect("IOrderService.Get")]
+        [PerformanceAspect(5)]
         public async Task<IResult> Update(Order entity)
         {
             await _orderDal.Update(entity);
