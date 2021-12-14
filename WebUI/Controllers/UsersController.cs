ec8e565db0694de473258accffedac70fbd81549
emir57 <emir.gurbuz06@hotmail.com>
2021-12-14 18:04:04 +0300

    Update user

---
+++ WebUI/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Security.Hashing;
+using Core.Utilities.Results;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost]
+        [Route("update")]
+        public async Task<IActionResult> UpdateUser(UpdateUserDto updateUserDto)
+        {
+            var user = await _userService.Get(x => x.Id == updateUserDto.UserId);
+            if (!HashingHelper.VerifyPasswordHash(updateUserDto.Password, user.Data.PasswordHash, user.Data.PasswordSalt))
+            {
+                return Ok(new ErrorResult(Messages.WrongPassword));
+            }
+            user.Data.FirstName = updateUserDto.FirstName;
+            user.Data.LastName = updateUserDto.LastName;
+            var result = await _userService.Update(user.Data);
+            if (!result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return Ok(result);
+        }
+    }
+}
