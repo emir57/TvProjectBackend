2502c98e3f5ad0fe4b07b600d3d325b30b1ad061
emir57 <emir.gurbuz06@hotmail.com>
2021-12-14 21:48:19 +0300

    Fix problem

--- DataAccess/Concrete/EntityFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -28,7 +28,7 @@ namespace DataAccess.Concrete.EntityFramework
                              on a.CityId equals c.Id
                              select new OrderDto
                              {
-                                 Id=o.Id
+                                 Id=o.Id,Fix
                                  TotalPrice = o.TotalPrice,
                                  ShippedDate = o.ShippedDate,
                                  User = u,
