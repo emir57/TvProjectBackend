a45f76b0f0a81e6dcdd59498f601014d815a413d
emir57 <emir.gurbuz06@hotmail.com>
2021-12-25 00:58:40 +0300

    Role Controller

---
+++ WebUI/Controllers/RolesController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+        [HttpGet]
+        [Route("getall")]
+        public async Task<ActionResult> GetRoles()
+        {
+            var result = await _roleService.GetAll();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
