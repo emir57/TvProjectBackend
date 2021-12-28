0ee0699ccac6d6b36aaac551ce43f449ece967eb
emir57 <emir.gurbuz06@hotmail.com>
2021-12-28 19:13:47 +0300

    User Address City Model

---
+++ Entities/Dtos/UserAddressCityCto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Dtos
+{
+    public class UserAddressCityCto:IDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string AddressText { get; set; }
+        public byte CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
