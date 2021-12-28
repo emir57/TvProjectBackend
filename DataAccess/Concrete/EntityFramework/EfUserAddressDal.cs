40e18ac60e6adc9e5f8149a112378eccef75e19b
emir57 <emir.gurbuz06@hotmail.com>
2021-12-28 19:20:36 +0300

    Get User Address By CityName

--- DataAccess/Concrete/EntityFramework/EfUserAddressDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserAddressDal.cs
@@ -1,15 +1,37 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.Dtos;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfUserAddressDal : EfEntityRepositoryBase<UserAddress, TvProjectContext>, IUserAddressDal
     {
-        
+        public async Task<List<UserAddressCityDto>> GetAddressByCityName(Expression<Func<UserAddressCityDto, bool>> filter)
+        {
+            using(var context = new TvProjectContext())
+            {
+                var result = from adress in context.UserAddresses
+                             from city in context.Cities
+                             where adress.CityId == city.Id
+                             select new UserAddressCityDto
+                             {
+                                 Id = adress.Id,
+                                 UserId = adress.UserId,
+                                 CityId = adress.CityId,
+                                 AddressText = adress.AddressText,
+                                 CityName = city.CityName
+                             };
+                return await result.Where(filter).ToListAsync();
+
+            }
+        }
     }
 }
