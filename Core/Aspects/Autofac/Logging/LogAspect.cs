d300ad85061dbda2c8ba2f82150558047e592d61
emir57 <emir.gurbuz06@hotmail.com>
2022-01-08 14:17:34 +0300

    LogAspect

---
+++ Core/Aspects/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,48 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Logging
+{
+    public class LogAspect:MethodInterception
+    {
+        private LoggerServiceBase _loggerService;
+        public LogAspect(Type loggerService)
+        {
+            if (!typeof(LoggerServiceBase).IsAssignableFrom(loggerService))
+            {
+                throw new Exception(AspectMessages.WrongLoggingType);
+            }
+            _loggerService = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _loggerService.Info(GetLogDetail(invocation));
+        }
+
+        private LogDetail GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = new List<LogParameter>();
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logParameters.Add(new LogParameter
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Type = invocation.Arguments[i].GetType().ToString(),
+                    Value = invocation.Arguments[i]
+                });
+            }
+            var logDetail = new LogDetail
+            {
+                MethodName = invocation.Method.Name,
+                LogParameters = logParameters
+            };
+            return logDetail;
+        }
+    }
+}
