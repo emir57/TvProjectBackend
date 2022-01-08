5ccaad5d65f0b858a32e345ea41edcc6a6413a93
emir57 <emir.gurbuz06@hotmail.com>
2022-01-08 14:25:06 +0300

    ExceptionLogAspect

---
+++ Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,50 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Exception
+{
+    public class ExceptionLogAspect:MethodInterception
+    {
+        private LoggerServiceBase _loggerServiceBase;
+        public ExceptionLogAspect(Type loggerSerivce)
+        {
+            if (!typeof(LoggerServiceBase).IsAssignableFrom(loggerSerivce))
+            {
+                throw new System.Exception(AspectMessages.WrongLoggingType);
+            }
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerSerivce);
+        }
+        protected override void OnException(IInvocation invocation, System.Exception exception)
+        {
+            LogDetailWithException logDetailWithException = GetLogDetail(invocation);
+            logDetailWithException.ExceptionMessage = exception.Message;
+            _loggerServiceBase.Error(logDetailWithException);
+        }
+
+        private LogDetailWithException GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = new List<LogParameter>();
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logParameters.Add(new LogParameter
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Type = invocation.Arguments[i].GetType().ToString(),
+                    Value = invocation.Arguments[i]
+                });
+            }
+            var logDetailWithException = new LogDetailWithException
+            {
+                MethodName = invocation.Method.Name,
+                LogParameters = logParameters
+            };
+            return logDetailWithException;
+        }
+    }
+}
