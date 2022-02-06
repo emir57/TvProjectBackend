9e91bef29ef6c3c832383de8b06875e142a9c647
emir57 <emir.gurbuz06@hotmail.com>
2022-02-06 14:03:53 +0300

    CreditCardWithUserDto

---
+++ Entities/Dtos/CreditCardWithUserDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Dtos
+{
+    public class CreditCardWithUserDto:IDto
+    {
+        public UserCreditCard UserCreditCard { get; set; }
+        public User User { get; set; }
+    }
+}
