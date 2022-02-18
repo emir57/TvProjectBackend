844dbb7bcd7b9c8e115bede62e445c04d7d8e020
emir57 <emir.gurbuz06@hotmail.com>
2022-02-18 22:58:06 +0300

    refactoring

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -23,12 +23,12 @@ namespace Core.Aspects.Autofac.Validation
         }
         protected override void OnBefore(IInvocation invocation)
         {
-            var context = (IValidator)Activator.CreateInstance(_validatorType);
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
             var entityType = _validatorType.BaseType.GetGenericArguments()[0];
             var entities = invocation.Arguments.Where(x => x.GetType() == entityType);
             foreach (var entity in entities)
             {
-                ValidationTool.Validate(context, entity);
+                ValidationTool.Validate(validator, entity);
             }
         }
     }
