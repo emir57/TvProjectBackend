60901bac45e601b9c5574fea57c732acd42db0e3
emir57 <emir.gurbuz06@hotmail.com>
2022-02-18 22:39:08 +0300

    Refactoring

--- Core/CrossCuttingConcerns/Logging/LogDetailWithException.cs
+++ Core/CrossCuttingConcerns/Logging/LogDetailWithException.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Core.CrossCuttingConcerns.Logging.Log4Net
+namespace Core.CrossCuttingConcerns.Logging
 {
     public class LogDetailWithException:LogDetail
     {
