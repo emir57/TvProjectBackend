be8bfc6ffa56501f95ee342005fae166e2b861f1
emir57 <emir.gurbuz06@hotmail.com>
2022-02-26 21:19:15 +0300

    UpdateUserDto and User map

--- Entities/Mapper/AutoMapper/MappingProfileEntity.cs
+++ Entities/Mapper/AutoMapper/MappingProfileEntity.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Core.Entities.Concrete;
+using Entities.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +9,12 @@ namespace Entities.Mapper.AutoMapper
 {
     public class MappingProfileEntity : Profile
     {
+        public MappingProfileEntity()
+        {
+            CreateMap<UpdateUserDto, User>()
+                .ForMember(member => member.Id, member2 => member2.MapFrom(x => x.UserId));
+            CreateMap<User, UpdateUserDto>()
+                .ForMember(member => member.UserId, member2 => member2.MapFrom(x => x.Id));
+        }
     }
 }
