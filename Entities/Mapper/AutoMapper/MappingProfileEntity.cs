0b26115f96bcedffd1823e6400bad447676662f8
emir57 <emir.gurbuz06@hotmail.com>
2022-02-26 21:13:46 +0300

    add mapping profile entity

---
+++ Entities/Mapper/AutoMapper/MappingProfileEntity.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Mapper.AutoMapper
+{
+    public class MappingProfileEntity : Profile
+    {
+    }
+}
