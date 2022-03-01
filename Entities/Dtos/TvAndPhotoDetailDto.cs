a9997a219a48646bbc077b52bf238cce470e52e7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-01 17:25:00 +0300

    Refactoring TvAndPhotoDetailDto

--- Entities/Dtos/TvAndPhotoDetailDto.cs
+++ Entities/Dtos/TvAndPhotoDetailDto.cs
@@ -8,10 +8,7 @@ namespace Entities.Dtos
 {
     public class TvAndPhotoDetailDto:IDto
     {
-        public TvAndPhotoDetailDto()
-        {
-            Photos = new List<Photo>();
-        }
+        
         public int Id { get; set; }
         public string ProductName { get; set; }
         public string ProductCode { get; set; }
