41d7637feda0166bc075c41e687e904386f4bf7f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 22:40:41 +0300

    Refactoring EfRoleDal

--- DataAccess/Concrete/EntityFramework/EfRoleDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRoleDal.cs
@@ -13,36 +13,34 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRoleDal : EfEntityRepositoryBase<Role, TvProjectContext>, IRoleDal
     {
+        private readonly TvProjectContext _context;
+
+        public EfRoleDal(TvProjectContext context)
+        {
+            _context = context;
+        }
+
         public async Task AddUserRoleAsync(User user, Role role)
         {
-            using(var context = new TvProjectContext())
-            {
                 var userRole = new UserRole
                 {
                     RoleId = role.Id,
                     UserId = user.Id
                 };
-                context.UserRoles.Add(userRole);
-                await context.SaveChangesAsync();
-            }
+                _context.UserRoles.Add(userRole);
+                await _context.SaveChangesAsync();
         }
 
         public async Task<UserRole> GetUserRoleAsync(Expression<Func<UserRole, bool>> filter)
         {
-            using(var context = new TvProjectContext())
-            {
-                return await context.UserRoles.SingleOrDefaultAsync(filter);
-            }
+                return await _context.UserRoles.SingleOrDefaultAsync(filter);
         }
 
         public async Task RemoveUserRoleAsync(User user, Role role)
         {
-            using (var context = new TvProjectContext())
-            {
-                var userRole = await context.UserRoles.SingleOrDefaultAsync(x => x.RoleId == role.Id && x.UserId == user.Id);
-                context.UserRoles.Remove(userRole);
-                await context.SaveChangesAsync();
-            }
+                var userRole = await _context.UserRoles.SingleOrDefaultAsync(x => x.RoleId == role.Id && x.UserId == user.Id);
+                _context.UserRoles.Remove(userRole);
+                await _context.SaveChangesAsync();
         }
     }
 }
