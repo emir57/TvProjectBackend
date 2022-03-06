721b76beef6521ddd627c04b75b746e3acec6de3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-06 18:46:45 +0300

    Refactoring Messages

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,47 +12,30 @@ namespace Business.Constants
         public static string SuccessDelete = "Silme Başarılı";
         public static string SuccessUpdate = "Güncelleme Başarılı";
         public static string SuccessGet = "Listeleme Başarılı";
-
         public static string AuthorizationDenied = "Yetkisiz işlem";
         public static string AccessTokenCreated = "Token Başarıyla Oluşturuldu";
-
-
         public static string UserNotFound = "Kullanıcı bulunamadı";
         public static string WrongPassword = "Kullanıcı adı veya parola yanlış";
         public static string SuccessfulLogin = "Giriş Başarılı";
-
         public static string AlreadyUserExists = "Böyle bir kullanıcı zaten var";
         public static string SuccessfulRegister = "Kayıt işlemi başarılı";
-
         public static string CreateUserAddress = "Adres başarıyla oluşturuldu";
         public static string DeleteUserAddress = "Adres başarıyla silindi";
         public static string UpdateUserAddress = "Adres başarıyla güncellendi";
-
         public static string UpdateCreditCard = "Kredi kartı başarıyla güncellendi";
         public static string DeleteUserCreditCard = "Kredi kartı başarıyla silindi";
         public static string AddUserCreditCard = "Kredi kartı başarıyla eklendi";
-
         public static string UploadImage = "Başarıyla Yüklendi";
         public static string IsMainExists = "Is Main zaten mevcut lütfen işareti kaldırıp yükleyiniz";
-
         public static string ProductAlreadyExists = "Böyle bir ürün zaten var";
-
         public static string SuccessfulChangePassword = "Parola başarıyla sıfırlandı";
-
         public static string SuccessfulUserUpdate = "Başarıyla Güncellendi";
-
         public static string OrderIsNotFound = "Sipariş Bulunamadı";
-
         public static string CardIsNotFound = "Kredi Kartı Bulunamadı";
-
         public static string AddressIsNotFound = "Adres Bulunamadı";
-
         public static string CityIsNotFound = "Şehir Bulunamadı";
-
         public static string RoleNotFound = "Rol Bulunamadı";
-
         public static string DontChangeAdminRole = "Admin Rolü Değiştirilemez";
-
         public static string UserAddressMaximumCount6 = "En fazla 6 tane adres eklenebilir";
     }
 }
--- Core/Utilities/Messages/AspectMessages.cs
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -8,7 +8,6 @@ namespace Core.Utilities.Messages
     public static class AspectMessages
     {
         public static string WrongLoggingType = "Loglama için geçersiz tip";
-
         public static string WrongValidationType = "Validasyon için geçersiz tip";
     }
 }
