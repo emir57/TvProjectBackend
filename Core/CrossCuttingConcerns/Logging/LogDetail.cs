ee98c939216d14f1077918b76539efff0a0f1d3e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-09 16:45:39 +0300

    add user roles

--- Core/CrossCuttingConcerns/Logging/LogDetail.cs
+++ Core/CrossCuttingConcerns/Logging/LogDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +10,6 @@ namespace Core.CrossCuttingConcerns.Logging
         public string MethodName { get; set; }
         public List<LogParameter> LogParameters { get; set; }
         public string UserEmail { get; set; }
+        public List<Role> UserRoles { get; set; }
     }
 }
