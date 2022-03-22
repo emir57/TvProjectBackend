2c0a1791779bca9d76d60244b5802f47f679b946
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-22 17:37:36 +0300

    fix id

--- WebUI/Controllers/UsersController.cs
+++ WebUI/Controllers/UsersController.cs
@@ -59,7 +59,7 @@ namespace WebUI.Controllers
         [Route("updateAdmin")]
         public async Task<IActionResult> UpdateUserAdmin(UpdateUserAdminDto updateUserAdminDto)
         {
-            var findUser = await _userService.GetByIdAsync(updateUserAdminDto.UserId);
+            var findUser = await _userService.GetByIdAsync(updateUserAdminDto.Idfix);
             if(findUser.Data == null)
             {
                 return BadRequest(Messages.UserNotFound);
