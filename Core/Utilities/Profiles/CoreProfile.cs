0a5238d3811a35f7ab1472dc256e9029679b6c61
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-24 19:19:31 +0300

    Create Mapping

--- Core/Utilities/Profiles/CoreProfile.cs
+++ Core/Utilities/Profiles/CoreProfile.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
 
 namespace Core.Utilities.Profiles
 {
     public class CoreProfile : Profile
     {
+        public CoreProfile()
+        {
+            CreateMap<User, UserForRegisterDto>();
+            CreateMap<UserForRegisterDto, User>();
+        }
     }
 }
