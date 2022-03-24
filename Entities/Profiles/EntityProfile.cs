156cc7c08c4178bc245e74959a096e2dec0937da
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-24 18:57:16 +0300

    EntityProfile

---
+++ Entities/Profiles/EntityProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Profiles
+{
+    public class EntityProfile : Profile
+    {
+        public EntityProfile()
+        {
+            CreateMap<User, LoginingUser>();
+            CreateMap<LoginingUser,User>();
+        }
+    }
+}
