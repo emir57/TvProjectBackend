9bfa584c696cbbe5587237bafc51edd87d90c7a9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-23 22:34:59 +0300

    GetBrandsWithCountAsync

--- DataAccess/Abstract/ITvBrandDal.cs
+++ DataAccess/Abstract/ITvBrandDal.cs
@@ -4,11 +4,12 @@ using Entities.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
     public interface ITvBrandDal:IEntityRepository<TvBrand>
     {
-        List<CategoryWithCountDto> GetBrandsWithCount();
+        Task<List<CategoryWithCountDto>> GetBrandsWithCountAsync();
     }
 }
--- DataAccess/Concrete/EntityFramework/EfTvBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfTvBrandDal.cs
@@ -1,13 +1,30 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfTvBrandDal:EfEntityRepositoryBase<TvBrand,TvProjectContext>,ITvBrandDal
+    public class EfTvBrandDal : EfEntityRepositoryBase<TvBrand, TvProjectContext>, ITvBrandDal
     {
+        public async Task<List<CategoryWithCountDto>> GetBrandsWithCountAsync()
+        {
+            using (var context = new TvProjectContext())
+            {
+                var result = from br in context.TvBrands
+                             select new CategoryWithCountDto
+                             {
+                                 Name = br.Name,
+                                 Count = context.Tvs.Where(t => t.BrandId == br.Id).Count()
+                             };
+                return await result.ToListAsync();
+            }
+        }
     }
 }
