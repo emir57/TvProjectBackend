f30141b7e6024808ab128767530bd73cdc30f2b4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-24 16:45:28 +0300

    Create OrderValidator

---
+++ Business/Validators/FluentValidation/OrderValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Validators.FluentValidation
+{
+    public class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(o => o.UserId).NotEmpty().NotNull();
+            RuleFor(o => o.TvId).NotEmpty().NotNull();
+            RuleFor(o => o.TotalPrice).NotEmpty().NotNull();
+        }
+    }
+}
