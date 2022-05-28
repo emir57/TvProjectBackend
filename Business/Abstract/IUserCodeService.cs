37ef54254ecf9dff4e591f677d321cf7c837832c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-28 19:51:58 +0300

    IUserCodeService

---
+++ Business/Abstract/IUserCodeService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserCodeService
+    {
+        Task<IDataResult<UserCode>> GetByUserIdAysnc(int userId);
+        Task<IResult> AddAsync(UserCode userCode);
+        Task<IResult> DeleteAsync(UserCode userCode);
+        Task<IResult> DeleteAsync(int userCodeId);
+        Task<IResult> DeleteByUserIdAsync(int userId);
+        Task<IResult> UpdateAsync(UserCode userCode);
+    }
+}
