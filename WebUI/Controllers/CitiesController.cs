db4c9ad1fc53793954511bf5cc4d78ad739363e3
emir57 <emir.gurbuz06@hotmail.com>
2022-07-03 15:21:23 +0300

    CitiesController remove unused using

--- WebUI/Controllers/CitiesController.cs
+++ WebUI/Controllers/CitiesController.cs
@@ -1,12 +1,8 @@
 ﻿using Business.Abstract;
-using Business.Constants;
 using Core.Utilities.Results;
 using Entities.Concrete;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
