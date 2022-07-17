10f57772f7159face1fc3d1bbf4cd3644eb993e9
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 00:06:39 +0300

    BrandManagerTests

---
+++ Business.Tests/BrandManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Moq;
+using System.Collections.Generic;
+
+namespace Business.Tests
+{
+    public class BrandManagerTests
+    {
+        Mock<ITvBrandService> _mockBrandService;
+        List<TvBrand> _dbBrand;
+        public BrandManagerTests()
+        {
+            _mockBrandService = new Mock<ITvBrandService>();
+            _dbBrand = new List<TvBrand>
+            {
+                new TvBrand{Id=1,Name="Samsung",Address="",PhoneNumber=""},
+                new TvBrand{Id=2,Name="Philips",Address="",PhoneNumber=""},
+                new TvBrand{Id=3,Name="TCL",Address="",PhoneNumber=""},
+                new TvBrand{Id=3,Name="LG",Address="",PhoneNumber=""},
+            };
+        }
+    }
+}
