2c79fdc4cc384c6b475aab27d3cd583462e91130
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 00:09:06 +0300

    Get_all_brands test

--- Business.Tests/BrandManagerTests.cs
+++ Business.Tests/BrandManagerTests.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using Entities.Concrete;
 using Moq;
 using System.Collections.Generic;
+using Xunit;
 
 namespace Business.Tests
 {
@@ -17,8 +19,16 @@ namespace Business.Tests
                 new TvBrand{Id=1,Name="Samsung",Address="",PhoneNumber=""},
                 new TvBrand{Id=2,Name="Philips",Address="",PhoneNumber=""},
                 new TvBrand{Id=3,Name="TCL",Address="",PhoneNumber=""},
-                new TvBrand{Id=3,Name="LG",Address="",PhoneNumber=""},
+                new TvBrand{Id=4,Name="LG",Address="",PhoneNumber=""},
             };
+            _mockBrandService.Setup(x => x.GetListAsync()).ReturnsAsync(new SuccessDataResult<List<TvBrand>>(_dbBrand));
+        }
+
+        [Fact]
+        public async void Get_all_brands()
+        {
+            var brands = await _mockBrandService.Object.GetListAsync();
+            Assert.Equal(brands.Data.Count, 4);
         }
     }
 }
