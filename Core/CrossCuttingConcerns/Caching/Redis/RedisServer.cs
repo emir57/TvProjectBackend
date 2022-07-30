f6cd2e176c279212874ccbae2b1bd4bcb1618ca6
emir57 <emir.gurbuz06@hotmail.com>
2022-07-30 21:52:14 +0300

    Create RedisServer

---
+++ Core/CrossCuttingConcerns/Caching/Redis/RedisServer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Caching.Redis
+{
+    public class RedisServer
+    {
+        private ConnectionMultiplexer _connectionMultiplexer;
+        private IDatabase _database;
+        private string configurationString;
+        private int _currentDatabaseId = 0;
+        public RedisServer(IConfiguration configuration)
+        {
+            createRedisConfigurationString(configuration);
+
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(configurationString);
+            _database = _connectionMultiplexer.GetDatabase(_currentDatabaseId);
+        }
+
+        public IDatabase Database => _database;
+        public IEnumerable<RedisKey> Keys(string pattern)
+            => _connectionMultiplexer.GetServer(configurationString).Keys(_currentDatabaseId, pattern);
+
+        public void FlushDatabase()
+        {
+            _connectionMultiplexer.GetServer(configurationString).FlushDatabase(_currentDatabaseId);
+        }
+
+        private void createRedisConfigurationString(IConfiguration configuration)
+        {
+            string host = configuration.GetSection("RedisConfiguration:host").Value;
+            string port = configuration.GetSection("RedisConfiguration:port").Value;
+            configurationString = String.Format($"{host}:{port}");
+        }
+    }
+}
