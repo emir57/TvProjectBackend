6e4d13f833676a8634b3abc042309726d246752b
emir57 <emir.gurbuz06@hotmail.com>
2022-08-24 14:42:50 +0300

    Get all tv count 4 Test

--- Business.Tests/TvManagerTests.cs
+++ Business.Tests/TvManagerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
+using Business.Concrete;
 using Core.Utilities.Results;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Moq;
 using System.Collections.Generic;
@@ -10,12 +12,12 @@ namespace Business.Tests
 {
     public class TvManagerTests
     {
-        Mock<ITvService> _mockTvService;
+        Mock<ITvDal> _mockTvDal;
         List<Tv> _dbTv;
         IDataResult<List<Tv>> _result;
         public TvManagerTests()
         {
-            _mockTvService = new Mock<ITvService>();
+            _mockTvDal = new Mock<ITvDal>();
             _dbTv = new List<Tv>()
             {
                 new Tv{Id=1,ProductName="Samsung",BrandId=1,ScreenInch="49",ScreenType="QLED"},
@@ -23,30 +25,16 @@ namespace Business.Tests
                 new Tv{Id=3,ProductName="Lg",BrandId=2,ScreenInch="49",ScreenType="QLED"},
                 new Tv{Id=4,ProductName="TCL",BrandId=1,ScreenInch="49",ScreenType="QLED"},
             };
-            _mockTvService.Setup(m => m.GetListAsync()).ReturnsAsync(new SuccessDataResult<List<Tv>>(_dbTv));
-            _mockTvService.Setup(m => m.GetListByBrandAsync(It.IsAny<int>())).ReturnsAsync((int i) => new SuccessDataResult<List<Tv>>(_dbTv.Where(x => x.BrandId == i).ToList()));
-            _mockTvService.Setup(m => m.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((int i) => new SuccessDataResult<Tv>(_dbTv.SingleOrDefault(x => x.Id == i)));
         }
         [Fact]
-        public async void Get_all_tvs()
+        public async void Get_all_tv_count_4()
         {
-            var list = await _mockTvService.Object.GetListAsync();
-            Assert.Equal(list.Data.Count, 4);
-        }
-        [Theory]
-        [InlineData(2)]
-        public async void Get_tvs_by_brand_id(int brandId)
-        {
-            var list = await _mockTvService.Object.GetListByBrandAsync(brandId);
-            Assert.Equal(list.Data.Count, 1);
-        }
+            _mockTvDal.Setup(x => x.GetAllAsync()).ReturnsAsync(_dbTv);
 
-        [Theory]
-        [InlineData(1)]
-        public async void Get_tv_by_id(int id)
-        {
-            var tv = await _mockTvService.Object.GetByIdAsync(id);
-            Assert.NotEqual(tv.Data, null);
+            TvManager tvManager = new TvManager(_mockTvDal.Object);
+            IDataResult<List<Tv>> list = await tvManager.GetListAsync();
+
+            Assert.Equal(list.Data.Count, 4);
         }
     }
 }
