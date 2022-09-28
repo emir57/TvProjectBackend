6d400084a411c8cd309a0baa4d86c9a0ca8e3916
emir57 <emir.gurbuz06@hotmail.com>
2022-09-28 14:21:59 +0300

    Create TvProjectConfiguration

---
+++ DataAccess/Contexts/TvProjectConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using EFCache;
+using System.Data.Entity.Core.Common;
+
+namespace DataAccess.Contexts
+{
+    public class TvProjectConfiguration : System.Data.Entity.DbConfiguration
+    {
+        public TvProjectConfiguration()
+        {
+            var t = new CacheTransactionHandler(new InMemoryCache());
+            AddInterceptor(t);
+            var cachingPolicy = new CachingPolicy();
+            Loaded += (sender, args) =>
+                args.ReplaceService<DbProviderServices>((s, _) => new CachingProviderServices(s, t, cachingPolicy));
+        }
+    }
+}
